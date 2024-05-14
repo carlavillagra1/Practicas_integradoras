09678b33b10be20f33dc20d43cf1e139e6f67ea8
Carla Villagra <villagraconce06@gmail.com>
2024-05-14 16:45:34 -0300

    modifique rutas

--- src/routes/product.routerMDB.js
+++ src/routes/product.routerMDB.js
@@ -1,53 +1,56 @@
 const express = require("express");
-const productManager = require("../dao/producManagerMDB")
+const productManager = require("../dao/producManagerMDB.js")
 
 
 const router = express.Router()
 
-router.get('/', async(req,res) =>{
+router.get('/', async (req, res) => {
     try {
         const products = await productManager.readProducts()
         res.send({ result: "success", playload: products })
     } catch (error) {
-        res.status(404).json({message: "No se encontraron los productos"})        
+        res.status(404).json({ message: "No se encontraron los productos" })
     }
 })
-router.get('/id', async(req,res) =>{
+router.get('/id', async (req, res) => {
     try {
         const product = await productManager.getProductById(id)
         res.send({ result: "success", playload: product })
     } catch (error) {
-        res.status(404).json({message: "No se encontro el producto"})        
+        res.status(404).json({ message: "No se encontro el producto" })
     }
 })
 
-router.post('/', async(req,res)=>{
+router.post('/', async (req, res) => {
     try {
-        const result = await productManager.createProduct()
-        res.send({result: "success", playload: result})
-        
+        const { title, description, price, thumbnail, code, stock } = req.body
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            return res.status(400).json({ message: "Todos los campos son obligatorios" });
+        }
+        const result = await productManager.createProduct(title, description, price, thumbnail, code, stock);
+        res.send({ result: "success", payload: result });
+
     } catch (error) {
-        res.status(404).json({message: "No se agrego el producto"})
-        
+        res.status(500).json({ message: "No se pudo agregar el producto" });
     }
-})
-router.put('/id', async(req,res) =>{
+});
+router.put('/id', async (req, res) => {
     try {
         let { id } = req.params
         const result = await productManager.updateProduct(id)
-        res.send({result: "success", playload: result})
-        
+        res.send({ result: "success", playload: result })
+
     } catch (error) {
-        res.status(404).json({message: "No se pudo modificar el producto"})
+        res.status(404).json({ message: "No se pudo modificar el producto" })
     }
 })
-router.delete('/id', async(req,res) =>{
+router.delete('/id', async (req, res) => {
     try {
         let { id } = req.params
         const result = await productManager.deleteProduct(id)
         res.send({ result: "success", playload: result })
     } catch (error) {
-        res.status(404).json({message: "No se borro el producto"})
+        res.status(404).json({ message: "No se borro el producto" })
     }
 })
 module.exports = router
